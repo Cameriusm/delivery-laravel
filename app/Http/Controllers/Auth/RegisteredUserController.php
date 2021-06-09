de9998ebf92b383819ee24f4e6c9f3d7a890bd46
unknown <cameriusm@gmail.com>
2021-06-09 07:37:17 +0700

    Update migrations

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -34,13 +34,19 @@ class RegisteredUserController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' => 'required|string|max:255',
+            'first_name' => 'required|string|max:255',
+            'second_name' => 'required|string|max:255',
+            'address' => 'string|max:255',
+            'phone_number' => 'string|max:11',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
-            'name' => $request->name,
+            'first_name' => $request->first_name,
+            'second_name' => $request->second_name,
+            'address' => $request->address,
+            'phone_number' => $request->phone_number,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
